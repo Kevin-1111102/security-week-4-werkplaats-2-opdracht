c82bfe2e5dcaacdb8aba4893288893974cf4acbf
kevin-1111102 <97698002+Kevin-1111102@users.noreply.github.com>
2025-10-07 20:37:19 +0200

    this code changed the old hash and salt
    the hash was not secure  and the salt was hardcoded and less secure so i changed it to make it less secure

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,8 +1,9 @@
-import hashlib
+from werkzeug.security import generate_password_hash, check_password_hash
 
-
+#secures the hash with unique salt
 def hash_password(password):
-    # TODO Static salt for simplicity (replace with dynamic salting for better security)
-    salt = "static_salt_12345"
-    """Hash the password with a static salt."""
-    return hashlib.sha256((salt + password).encode()).hexdigest()
+    return generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+
+ #checks if the password is correct
+def verify_password(stored_hash, password):
+    return check_password_hash(stored_hash, password)
